73292c6337407af63fee47aa22cf54b74569307a
protocol10 <akshaymukadam29@gmail.com>
2014-08-09 08:51:43 +0530

    Python samples and notes

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+import sys
+
+def main():
+    the_world_is_flat = 1
+    if the_world_is_flat:
+        print "Be Care full"
+
+if __name__ == '__main__':
+    main()
---
+++ stringfunc.py
@@ -0,0 +1,12 @@
+#! usr/bin/env python
+
+import sys
+
+def main():
+    word = 'python'
+    print word[0]
+    print word[0:3]
+    print 'print replacing thw world' + word[2:]
+    print len(word)
+if __name__ == '__main__':
+    main()
